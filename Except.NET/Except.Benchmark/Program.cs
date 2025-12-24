be81e4b40190bc04660eef995dc89042fade8a56
FLDBE6FN <florian-externe.lassalle@edvance.fr>
2025-12-24 18:28:33 +0100

    Add benchmark

--- Except.NET/Except.Benchmark/Program.cs
+++ Except.NET/Except.Benchmark/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+using static System.Excepts.Except;
+
+[MemoryDiagnoser(false)]
+class ExceptBenchmark
+{
+
+    double Divide(double a, double b)
+    {
+        Check(b != 0); // will throw an exception if b == 0
+
+        return a / b;
+    }
+
+    bool TryDivide(double a, double b, out double result)
+    {
+        try
+        {
+            result = Divide(10.0, 2.0);
+
+            return true;
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine(ex);
+        }
+
+        return false;
+    }
+
+    [Benchmark]
+    public void TryBlock()
+    {
+        TryDivide(10.0, 2.0, out var result);
+    }
+
+    [Benchmark]
+    public void TryInvoke()
+    {
+        Try(() => Divide(10.0, 2.0)).Catch(Throw);
+    }
+
+    [Benchmark]
+    public void TryDynamicInvoke()
+    {
+        Try(Divide, 10.0, 2.0).Catch(Throw);
+    }
+}
